d9d3519cbc4edfb78e7ad138082c2881f354f338
Rodion Ponomarenko <rodion.ponomarenko@customertimes.com>
2021-03-02 13:41:59 +0200

    mormilize scroll

--- App.js
+++ App.js
@@ -2,64 +2,56 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  Text, 
+  Text,
   View,
   ScrollView,
   TouchableOpacity,
   Image
 } from 'react-native';
-import LabelScroller from './LabelScroller'
-import data from './data.json'
+import LabelScroller from './LabelScroller';
+import data from './data.json';
 
 const App = () => {
-
-
   const pressOnItem = item => {
-    console.log('pressOnItem item',item)
-  }
+    console.log('pressOnItem item', item);
+  };
 
   return (
     <>
-      <SafeAreaView style={{flex:1}}>
-        <ScrollView contentContainerStyle={{flex:1,justifyContent:'center'}}>
-          <LabelScroller 
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <LabelScroller
             columns={3}
             data={data}
             renderItem={({ item, index }) => (
-              <TouchableOpacity
-                key={item.id}
-                onPress={() => pressOnItem(item)}
-                style={styles.item}
-              > 
-                <Image style={styles.itemImage} source={{uri:item.image}}/>
+              <TouchableOpacity key={item.id} onPress={() => pressOnItem(item)} style={styles.item}>
+                <Image style={styles.itemImage} source={{ uri: item.image }} />
                 <Text style={styles.itemLabel}>{item.label}</Text>
               </TouchableOpacity>
             )}
           />
-        </ScrollView>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  item:{
-    padding:5,
-    borderWidth:1,
+  item: {
+    padding: 5,
+    borderWidth: 1,
     borderColor: 'red',
-    borderRadius:10,
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection:'row',
-    margin:3
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    margin: 3
   },
-  itemLabel:{
-
-  },
-  itemImage:{
-    width:20,
-    height:20,
-    marginHorizontal:5
+  itemLabel: {},
+  itemImage: {
+    width: 20,
+    height: 20,
+    marginHorizontal: 5
   }
 });
 
--- LabelScroller.js
+++ LabelScroller.js
@@ -1,106 +1,130 @@
+import React, { useCallback, useMemo, useRef } from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  PanResponder,
+  Animated,
+  Dimensions
+} from 'react-native';
+import { divideArray } from './helper';
 
-import React,{useCallback,useMemo,useRef} from 'react';
-import { ScrollView, StyleSheet, Text, View, FlatList,PanResponder, Animated } from 'react-native'
-import {divideArray} from './helper'
-
-const LabelScroller = ({
-  data,
-  renderItem,
-  columns,
-  minLabelLength,
-  componentsArray
-}) => {
+const LabelScroller = ({ data, renderItem, columns, minLabelLength, componentsArray }) => {
   const scrollListRefs = useRef({});
 
-  if(!data) return null;
+  if (!data) return null;
 
-  if(data.length <= minLabelLength) {
+  if (data.length <= minLabelLength) {
     //render elems without scrolls
-    return <View style={styles.containerWithoutScrolling}>{data.map((item,index) => renderItem({item,index}))}</View>
+    return (
+      <View style={styles.containerWithoutScrolling}>
+        {data.map((item, index) => renderItem({ item, index }))}
+      </View>
+    );
   }
 
-  const panResponder = useRef(PanResponder.create({
-    //these things if for if user touch tochables button in children
-    onStartShouldSetPanResponder: () => true,
-    onMoveShouldSetPanResponder: () => true,
-    onStartShouldSetPanResponderCapture: () => false,
-    onMoveShouldSetPanResponderCapture: () => false,
-    onPanResponderTerminationRequest: () => true,
-    onShouldBlockNativeResponder: () => false,
+  const panResponder = useRef(
+    PanResponder.create({
+      //these things if for if user touch tochables button in children
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponderCapture: () => false,
+      onMoveShouldSetPanResponderCapture: () => false,
+      onPanResponderTerminationRequest: () => true,
+      onShouldBlockNativeResponder: () => false,
+      // When user moves element
+      onPanResponderMove: (event, gestureState) => {
+        //console.log('gestureState',gestureState.dx)
+        //const translatedX = Math.max(0, 0 + gestureState.dx);
+        const translatedX = gestureState.dx;
+        console.log('onPanResponderMove');
+        //console.log('scrollListRefs.current', scrollListRefs.current);
+        console.table(gestureState);
+        for (const row in scrollListRefs.current) {
+          const scrollRef = scrollListRefs.current[row];
+          //console.log('scrollRef.currentScrollPositonX', scrollRef.currentScrollPositonX);
+          const currentScrollPositonX = scrollRef.currentScrollPositonX || 0;
+          const currentScrollWidth = scrollRef.currentScrollWidth || 0;
+          //console.log('currentScrollPositonX', currentScrollPositonX);
+          //console.log('currentScrollWidth', currentScrollWidth);
 
-    onPanResponderMove: (event, gestureState) => {
-      //console.log('gestureState',gestureState.dx)
-      //const translatedX = Math.max(0, 0 + gestureState.dx);
-      const translatedX = gestureState.dx;
-      console.log('translatedX',translatedX)
-      for (const row in scrollListRefs.current) {
-        const scrollRef = scrollListRefs.current[row];
-        console.log('scrollRef.currentScrollPositonX',scrollRef.currentScrollPositonX)
-        const currentScrollPositonX = scrollRef.currentScrollPositonX || 0;
-        const newOffset = currentScrollPositonX +translatedX
-        console.log('scrollRef',scrollRef)
-        console.log('newOffset',newOffset)
-        scrollRef.scrollToOffset({offset:newOffset,animated:true})
+          const calculatedOffset = parseInt(
+            (translatedX * currentScrollWidth) / Dimensions.get('window').width / 3
+          );
+          console.log('calculatedOffset', calculatedOffset);
+          const newOffset =
+            translatedX < 0
+              ? currentScrollPositonX + Math.abs(calculatedOffset) // translatedX < 0 - user scrolls right
+              : currentScrollPositonX - Math.abs(calculatedOffset); // translatedX > 0 - user scrolls left
+
+          //console.log('scrollRef', scrollRef);
+          console.log('newOffset', newOffset);
+          scrollRef.scrollToOffset({ offset: newOffset, animated: true });
+        }
+      },
+      // When user drag ends
+      onPanResponderRelease: (e, gestureState) => {
+        //console.log('onPanResponderRelease');
+        //console.table(gestureState);
+        const overscrolledRight = gestureState.vx <= 0.06;
+        // console.log('overscrolledRight',overscrolledRight)
+
+        // console.log('scrollListRefs.current',scrollListRefs.current)
+        for (const row in scrollListRefs.current) {
+          const scrollRef = scrollListRefs.current[row];
+
+          //scrollRef.scrollToOffset({offset:300,animated:true})
+        }
+      },
+      // When user gesture is moved outside of direct parent
+      onPanResponderTerminate: (evt, gesture) => {
+        console.log('onPanResponderTerminate', gesture);
       }
-    },
-    onPanResponderRelease: (e, gesture) => {
-      //console.log('gesture',gesture)
-      const overscrolledRight = gesture.vx <= 0.06;
-     // console.log('overscrolledRight',overscrolledRight)
+    })
+  ).current;
 
-     // console.log('scrollListRefs.current',scrollListRefs.current)
-      for (const row in scrollListRefs.current) {
-        const scrollRef = scrollListRefs.current[row];
-
-        //scrollRef.scrollToOffset({offset:300,animated:true})
-      }
-    },
-  })).current;
-
-  const divided = useMemo(() => divideArray(data,columns),[data,columns]);
+  const divided = useMemo(() => divideArray(data, columns), [data, columns]);
 
   return (
     <View>
-      
-        <Animated.View
-          {...panResponder.panHandlers}
-        >
-          {
-            divided.map((rowData,i) => (
-              <FlatList
-                key={i.toString()}
-                ref={ref => scrollListRefs.current[i] = ref}
-                data={rowData}
-                renderItem={renderItem}
-                keyExtractor={item => item.id.toString()}
-                horizontal
-                showsVerticalScrollIndicator={false}
-                showsHorizontalScrollIndicator={false}
-                scrollEnabled={false}
-                scrollEventThrottle={16}
-                onScroll={(e) => {
-                  scrollListRefs.current[i].currentScrollPositonX = e.nativeEvent.contentOffset.x;
-                  //console.log('FlatList onScroll e',i, '  ',e.nativeEvent)
-                }}
-              />
-            ))
-          }
-        </Animated.View>
-      
+      <View {...panResponder.panHandlers}>
+        {divided.map((rowData, i) => (
+          <FlatList
+            key={i.toString()}
+            ref={ref => (scrollListRefs.current[i] = ref)}
+            data={rowData}
+            renderItem={renderItem}
+            keyExtractor={item => item.id.toString()}
+            horizontal
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            scrollEnabled={false}
+            scrollEventThrottle={2}
+            onScroll={e => {
+              scrollListRefs.current[i].currentScrollPositonX = e.nativeEvent.contentOffset.x;
+            }}
+            onContentSizeChange={contentWidth => {
+              scrollListRefs.current[i].currentScrollWidth = contentWidth;
+            }}
+          />
+        ))}
+      </View>
     </View>
-  )
-}
+  );
+};
 
 LabelScroller.defaultProps = {
   minLabelLength: 20,
-  columns:3
-}
+  columns: 3
+};
 
-export default LabelScroller
+export default LabelScroller;
 
 const styles = StyleSheet.create({
-  containerWithoutScrolling:{
-    flexDirection:'row',
-    flexWrap:'wrap'
+  containerWithoutScrolling: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   }
-})
+});
